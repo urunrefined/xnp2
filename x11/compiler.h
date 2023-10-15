b72b772dc619579f3320ab36862f9218f66e2f42
Tom Unbehau <xnp2@unrefined.xyz>
2023-10-15 18:34:57 +0200

    Cleanup some compiler.h flags
    Signed-off-by: Tom Unbehau <xnp2@unrefined.xyz>

--- x11/compiler.h
+++ x11/compiler.h
@@ -1,5 +1,5 @@
-#ifndef	NP2_X11_COMPILER_H__
-#define	NP2_X11_COMPILER_H__
+#ifndef NP2_X11_COMPILER_H__
+#define NP2_X11_COMPILER_H__
 
 /*-
  * Copyright (C) 2003, 2004 NONAKA Kimihiro <nonakap@gmail.com>
@@ -27,9 +27,9 @@
  */
 
 #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-#define	BYTESEX_BIG
+#define BYTESEX_BIG
 #elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define	BYTESEX_LITTLE
+#define BYTESEX_LITTLE
 #else
 #error Please set preprocessor macro __BYTE_ORDER__ to __ORDER_BIG_ENDIAN__ or __ORDER_LITTLE_ENDIAN__
 #endif
@@ -38,161 +38,124 @@
 
 #include <stdint.h>
 
-typedef	int32_t		SINT;
-typedef	uint32_t	UINT;
+typedef int32_t SINT;
+typedef uint32_t UINT;
 
-typedef	int8_t		SINT8;
-typedef	int16_t		SINT16;
-typedef	int32_t		SINT32;
-typedef	int64_t		SINT64;
+typedef int8_t SINT8;
+typedef int16_t SINT16;
+typedef int32_t SINT32;
+typedef int64_t SINT64;
 
-typedef	uint8_t		UINT8;
-typedef	uint16_t	UINT16;
-typedef	uint32_t	UINT32;
-typedef	uint64_t	UINT64;
+typedef uint8_t UINT8;
+typedef uint16_t UINT16;
+typedef uint32_t UINT32;
+typedef uint64_t UINT64;
 
-typedef	int			BOOL;
+typedef int BOOL;
 
 typedef signed long nxp2ptr;
-#define	INTPTR nxp2ptr
+#define INTPTR nxp2ptr
 
-#define PTR_TO_UINT32(p)	((unsigned int) (unsigned long) (p))
-#define UINT32_TO_PTR(v)	((void*)(unsigned long) ((UINT32)(v)))
+#define PTR_TO_UINT32(p) ((unsigned int)(unsigned long)(p))
+#define UINT32_TO_PTR(v) ((void *)(unsigned long)((UINT32)(v)))
 
-#ifndef	FALSE
-#define	FALSE	0
+#ifndef FALSE
+#define FALSE 0
 #endif
 
-#ifndef	TRUE
-#define	TRUE	(!FALSE)
+#ifndef TRUE
+#define TRUE (!FALSE)
 #endif
 
-//TODO: Sort this out later
-#define	MAX_PATH 4096
+// TODO: Sort this out later
+#define MAX_PATH 4096
 
-#ifndef	__cplusplus
-#ifndef	max
-#define	max(a,b)	(((a) > (b)) ? (a) : (b))
-#endif
-#ifndef	min
-#define	min(a,b)	(((a) < (b)) ? (a) : (b))
-#endif
-#endif	/* !__cplusplus */
-
-//TODO: Remove this and the ZeroMemory / FillMemory macros
+// TODO: Remove this and the ZeroMemory / FillMemory macros
 #include <string.h>
 
-#define	ZeroMemory(d,n)		memset((d), 0, (n))
-#define	FillMemory(a, b, c)	memset((a), (c), (b))
+#define ZeroMemory(d, n) memset((d), 0, (n))
+#define FillMemory(a, b, c) memset((a), (c), (b))
 
-#ifndef	roundup
-#define	roundup(x, y)	((((x)+((y)-1))/(y))*(y))
+#ifndef roundup
+#define roundup(x, y) ((((x) + ((y)-1)) / (y)) * (y))
 #endif
 
-#ifndef	NELEMENTS
-#define	NELEMENTS(a)	((int)(sizeof(a) / sizeof(a[0])))
+#ifndef NELEMENTS
+#define NELEMENTS(a) ((int)(sizeof(a) / sizeof(a[0])))
 #endif
 
-/* archtecture */
-/* amd64 */
-#if defined(amd64) || defined(__AMD64__) || defined(__amd64__) || \
-    defined(x86_64) || defined(__x86_64__) || defined(__X86_64__)
-#define	NP2_CPU_ARCH_AMD64
-#endif /* amd64 */
-/* i386 */
-#if defined(i386) || defined(__i386__) || defined(NP2_CPU_ARCH_AMD64)
-#define	NP2_CPU_ARCH_IA32
-#endif /* i386 */
+#define NP2_CPU_ARCH_AMD64
+#define NP2_CPU_ARCH_IA32
 
-#if defined(__GNUC__)
-#if defined(NP2_CPU_ARCH_IA32)
-#define	GCC_CPU_ARCH_IA32
-#endif
-#if defined(NP2_CPU_ARCH_AMD64)
-#define	GCC_CPU_ARCH_AMD64
-#endif
-#endif /* __GNUC__ */
+#define GCC_CPU_ARCH_IA32
+#define GCC_CPU_ARCH_AMD64
 
-#define	GETRAND()	random()
-#define	SPRINTF		sprintf
+#define GETRAND() random()
+#define SPRINTF sprintf
 
-#define	OEMCHAR		char
-#define OEMTEXT(s)	s
+#define OEMCHAR char
+#define OEMTEXT(s) s
 
-#if defined(CPUCORE_IA32)
 #define USE_FPU
-#if !defined(DISABLE_PC9821)
-#define	SUPPORT_PC9821
-#define	SUPPORT_CRT31KHZ
+#define SUPPORT_PC9821
+#define SUPPORT_CRT31KHZ
+#define SUPPORT_IDEIO
+#define SUPPORT_CRT15KHZ
+
+#define LOADINTELDWORD(a) (*((const UINT32 *)(a)))
+#define LOADINTELWORD(a) (*((const UINT16 *)(a)))
+#define STOREINTELDWORD(a, b) *(UINT32 *)(a) = (b)
+#define STOREINTELWORD(a, b) *(UINT16 *)(a) = (b)
+
+#ifndef FASTCALL
+#define FASTCALL
 #endif
-#define	SUPPORT_IDEIO
+#define CPUCALL FASTCALL
+#define MEMCALL FASTCALL
+#define DMACCALL FASTCALL
+#define IOOUTCALL FASTCALL
+#define IOINPCALL FASTCALL
+#define SOUNDCALL FASTCALL
+#define VRAMCALL FASTCALL
+#define SCRNCALL FASTCALL
+
+#ifdef DEBUG
+#define INLINE
+#define __ASSERT(s) assert(s)
 #else
-#define	SUPPORT_CRT15KHZ
+#ifndef __ASSERT
+#define __ASSERT(s)
 #endif
-
-#if defined(NP2_CPU_ARCH_IA32)
-#undef	MEMOPTIMIZE
-#define LOADINTELDWORD(a)	(*((const UINT32 *)(a)))
-#define LOADINTELWORD(a)	(*((const UINT16 *)(a)))
-#define STOREINTELDWORD(a, b)	*(UINT32 *)(a) = (b)
-#define STOREINTELWORD(a, b)	*(UINT16 *)(a) = (b)
-#if !defined(DEBUG) && !defined(NP2_CPU_ARCH_AMD64)
-#define	FASTCALL	__attribute__((regparm(2)))
-#endif	/* !DEBUG && !NP2_CPU_ARCH_AMD64 */
-#elif defined(arm) || defined (__arm__)
-#define	MEMOPTIMIZE	2
-#define	REG8		UINT
-#define	REG16		UINT
-#define	OPNGENARM
-#else
-#define	MEMOPTIMIZE	1
-#endif
-
-#ifndef	FASTCALL
-#define	FASTCALL
-#endif
-#define	CPUCALL		FASTCALL
-#define	MEMCALL		FASTCALL
-#define	DMACCALL	FASTCALL
-#define	IOOUTCALL	FASTCALL
-#define	IOINPCALL	FASTCALL
-#define	SOUNDCALL	FASTCALL
-#define	VRAMCALL	FASTCALL
-#define	SCRNCALL	FASTCALL
-
-#ifdef	DEBUG
-#define	INLINE
-#define	__ASSERT(s)	assert(s)
-#else
-#ifndef	__ASSERT
-#define	__ASSERT(s)
-#endif
-#ifndef	INLINE
-#define	INLINE		inline
+#ifndef INLINE
+#define INLINE inline
 #endif
 #endif
 
-#define	SUPPORT_24BPP
-#define	SUPPORT_32BPP
-#define	SUPPORT_NORMALDISP
+#ifndef __cplusplus
+#ifndef max
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef min
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+#endif /* !__cplusplus */
 
-#undef	SOUND_CRITICAL
-#undef	SOUNDRESERVE
-#define	SUPPORT_EXTERNALCHIP
+#define SUPPORT_24BPP
+#define SUPPORT_32BPP
+#define SUPPORT_NORMALDISP
 
-#define	SUPPORT_PC9861K
-#define	SUPPORT_HOSTDRV
+#define SUPPORT_EXTERNALCHIP
+#define SUPPORT_PC9861K
+#define SUPPORT_HOSTDRV
+#define SUPPORT_STATSAVE
+#define SUPPORT_S98
 
-#define	SUPPORT_STATSAVE
-
-#undef	SUPPORT_SASI
-#undef	SUPPORT_SCSI
-
-#define	SUPPORT_S98
+#undef SUPPORT_SASI
+#undef SUPPORT_SCSI
 
 #ifdef DEBUG
 #define TRACE
 #define TRACEVERBOSE
 #endif
 
-#endif	/* NP2_X11_COMPILER_H__ */
+#endif /* NP2_X11_COMPILER_H__ */
