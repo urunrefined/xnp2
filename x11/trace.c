f319d4b00cfc84df3d5f75f171e0e88e4de926df
Tom Unbehau <xnp2@unrefined.xyz>
2024-03-31 16:59:26 +0200

    Add hexdump and rename trace switches
    Signed-off-by: Tom Unbehau <xnp2@unrefined.xyz>

--- x11/trace.c
+++ x11/trace.c
@@ -1,4 +1,6 @@
 #include "compiler.h"
+
+#include <ctype.h>
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -8,4 +10,34 @@ void trace_fmt(const char *fmt, ...) {
     printf(fmt, ap);
     va_end(ap);
     printf("\n");
+}
+
+void hexdump(const uint8_t *buffer, size_t sz) {
+    for (size_t i = 0; i < sz; i += 16) {
+        printf("%08zX   ", i);
+
+        for (size_t j = 0; j < 16; j++) {
+            if (i + j < sz) {
+                printf("%02hhx ", buffer[i + j]);
+            } else {
+                printf("   ");
+            }
+        }
+
+        printf("  ");
+
+        for (size_t j = 0; j < 16; j++) {
+            if (i + j < sz) {
+                if (isprint(buffer[i + j])) {
+                    printf("%c", (char)buffer[i + j]);
+                } else {
+                    printf(".");
+                }
+            } else {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
 }
--- x11/trace.h
+++ x11/trace.h
@@ -1,6 +1,7 @@
 #ifndef NP2_X11_TRACE_H__
 #define NP2_X11_TRACE_H__
 
+#include <stddef.h>
 #include <stdio.h>
 
 #ifdef __cplusplus
@@ -8,14 +9,15 @@ extern "C" {
 #endif
 
 void trace_fmt(const char *fmt, ...);
+void hexdump(const uint8_t *buffer, size_t sz);
 
-#ifdef TRACE
+#ifdef ENABLE_TRACE
 #define TRACEOUT(arg) trace_fmt arg
 #else
 #define TRACEOUT(a)
 #endif
 
-#ifdef TRACEVERBOSE
+#ifdef ENABLE_VERBOSE
 #define VERBOSE(arg) trace_fmt arg
 #else
 #define VERBOSE(a)
